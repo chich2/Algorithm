c2bb06abcd13d9a7100ce9471c2acbcf6e1b4837
chiwonPark <bamtorrr@gmail.com>
2020-01-13 10:04:40 +0900

    have to be made for input file

--- max_num/max_num.py
+++ max_num/max_num.py
@@ -3,40 +3,22 @@
 T = int(input())            # 테스트 케이스 개수 T 입력받기
 print(T)
 for i in range(0, T):
-    while True:
+    while True:         # 10자리 입력하는 구간
         cnt = 0
-        list_temp = []          # 빈 리스트 생성
-        numberString = input()      # 10자리 한번에 입력받기
+        tmp = 0
+        numberString = input()      # 10자리 한 문장에 입력받기
         numberList_string = numberString.split()       # 나누어서 리스트 형태로 저장
-        # print(numberList_string)       # numberList 테스트용
         if len(numberList_string) is not 10:        # 10자리가 아니면 while문 재시작
             print("10자리를 입력하셔야해요.")
             continue
-
-            # while문 재시작하게 설정하는 방법을 몰라서 막힘 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-        for i in numberList_string:         # 리스트 요소 하나씩 숫자 범위 체크 후에 타당하면 빈 리스트에 추가하기
-            if 0 <= int(i) <= 10000:
-                list_temp.append(i)
+        for n in numberList_string:         # 리스트 요소 하나씩 숫자 범위 체크 후에 타당하면 빈 리스트에 추가하기
+            if 0 <= int(n) <= 10000:
+                if tmp < int(n):
+                    tmp = int(n)
                 cnt += 1
-                if cnt is 10:
-                    print(list_temp)  # 10개 제대로 출력되는지 확인용
-
             else:
                 print("0 이상 10000 이하의 정수만으로 10자리 다시 입력해주세요.")           # 숫자 제한 범위 초과시 cnt =0으로 만들어서
                 continue                                                                # break 불가하게 해서 while문 재시작하게 만들기
-
-
-
-
-
-    #     if 0 <= n and n <= 10000:
-    #         li.append(n)
-    #         cnt += 1
-    #     else:
-    #         print("0 이상 10000 이하의 정수를 입력해주세요")
-    #         continue
-    #     if cnt is 10:
-    #         break
-    # print(li)
+        if cnt is 10:
+            print('#' + str(i+1) + " " + str(tmp))
+            break
