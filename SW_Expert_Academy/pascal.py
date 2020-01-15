7ae375f7ae7c77e06e0c8c5d0128ebfdb8b44b03
chiwonPark <bamtorrr@gmail.com>
2020-01-15 17:00:43 +0900

    almost done

--- SW_Expert_Academy/pascal.py
+++ SW_Expert_Academy/pascal.py
@@ -2,15 +2,15 @@
 
 def make_pascal(scale):
     result = []
-    result.append([1])
-    for i in range(1, scale):
+#    result.append([1])
+    for i in range(1, scale+1):
         result_child = []
-        for j in range(i):
-            if j is 0 or i-j is 1:
+        for j in range(1, i+1):
+            if j is 1 or i==j:
                 result_child.append(1)
             else:
-                temp = int(result[i-1][j-1])
-                temp += int(result[i-1][j])
+                temp = int(result[i-2][j-2])        # 리스트: 0<= < n 이라는 것 명심!
+                temp += int(result[i-2][j-1])
                 result_child.append(temp)
         result.append(result_child)
     print(result)
