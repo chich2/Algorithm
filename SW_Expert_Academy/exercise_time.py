97ac82552cd448825f4d3612d1adba9d1bb96944
chiwonPark <bamtorrr@gmail.com>
2020-01-17 15:22:51 +0900

    done

---
+++ SW_Expert_Academy/exercise_time.py
@@ -0,0 +1,18 @@
+# swea 3431
+
+
+def calculate_times(para_times):
+    Low, Up, X_time = para_times[0], para_times[1], para_times[2]
+    if X_time > Up:
+        return -1
+    elif X_time < Low:
+        sub = Low - X_time
+        return sub
+    else:
+        return 0
+
+
+TestCase = int(input())
+for test_case in range(TestCase):
+    times = list(map(int,input().split()))
+    print("#{} {}".format(test_case + 1, calculate_times(times)))
