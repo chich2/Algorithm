83a0f662341fa5a9d94cdf388b484b91de38c5e6
chiwonPark <bamtorrr@gmail.com>
2020-01-21 16:58:48 +0900

    other person's answer

--- SW_Expert_Academy/possible_test_score.py
+++ SW_Expert_Academy/possible_test_score.py
@@ -3,11 +3,11 @@
 
 def main():
     tc = int(input())
-    for t in range(1, tc + 1):          # 전체 테스트 반복
+    for t in range(1, tc + 1):          # 전체 테스트 반복, t가 1부터 tc까지 보기좋게 만듬 => 겟
         score = [0]
-        vi = [0] * (100 * 100 + 1)
+        vi = [0] * (100 * 100 + 1)      # 조합이 최대 만개가 될 수 있다는 것인가?????
         n = input()
-        nums = list(map(int, input().split()))
+        nums = list(map(int, input().split()))      # nums : 받은 자연수
         for i in nums:
             for j in range(0, len(score)):
                 tmp = i + score[j]
