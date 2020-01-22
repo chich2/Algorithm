423c9820fede7ea482267f7a3f9e257d1ddc3b82
chiwonPark <bamtorrr@gmail.com>
2020-01-22 16:21:48 +0900

    done

---
+++ SW_Expert_Academy/min_gcd.py
@@ -0,0 +1,16 @@
+# 최대공약수 구하는 함수
+def gcd(numbers):
+    sorted_numbers = sorted(numbers)
+    for gcd_number in range(sorted_numbers[0]+1, 1, -1):
+        is_gcd_number = True
+        for compared_number in sorted_numbers:
+            if compared_number % gcd_number != 0:
+                is_gcd_number = False
+        if is_gcd_number == True:
+            return gcd_number
+
+
+# gcd 함수 테스트
+# list = [6,9,12]
+# print(gcd(list))
+
