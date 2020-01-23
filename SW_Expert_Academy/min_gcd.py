95376030560d59d0486111dd3a0af53fc2725905
chiwonPark <bamtorrr@gmail.com>
2020-01-23 10:43:25 +0900

    continue

--- SW_Expert_Academy/min_gcd.py
+++ SW_Expert_Academy/min_gcd.py
@@ -6,11 +6,22 @@ def gcd(numbers):
         for compared_number in sorted_numbers:
             if compared_number % gcd_number != 0:
                 is_gcd_number = False
-        if is_gcd_number == True:
+        if is_gcd_number:
             return gcd_number
- 
+
 
 # gcd 함수 테스트
 # list = [6,9,12]
 # print(gcd(list))
 
+
+def devide_numbers(numbers):
+    # sorted_numbers = sorted(numbers)
+    tmp = [0]
+    devided_numbers_1 = []
+    devided_numbers_2 = []
+
+    for number in numbers:
+        sw = True
+        for number_1 in devided_numbers_1:
+            if
