549405d0f072cf39be024f5f9eb624fd352e605d
chiwonPark <bamtorrr@gmail.com>
2020-02-06 16:57:38 +0900

    continue

--- Code_Ground/frog_jump.py
+++ Code_Ground/frog_jump.py
@@ -1,3 +1,13 @@
+
+
 number_of_testcases = int(input())
 for testcase in range(1, number_of_testcases +1):
-    print()
+    print("Case #{}".format(testcase))
+    number_of_rocks = int(input())
+    set_of_rocks = input().split()
+    set_of_rocks.insert(0,0)
+    max_jumping_lengh = int(input())
+    number_of_jumping = 0
+    current_position = 0
+
+    while True:
