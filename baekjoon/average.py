7a26da6a66f19d5ed6718708132a8ec98d6a4890
chich2 <bamtorrr@gmail.com>
2021-01-11 10:48:13 +0900

    210111

--- baekjoon/average.py
+++ baekjoon/average.py
@@ -1,2 +1,11 @@
 # 1546
-print("sth")
+# 정수를 나누어서 실수형으로 출력하는 방법
+# sum(), max()
+
+# 입력
+num_score = int(input())
+list_score = list(map(int, input().split()))
+
+# 계산    총합 * 100 / max / num_score
+ans = float(sum(list_score)) * 100 / max(list_score) / num_score
+print(ans)
