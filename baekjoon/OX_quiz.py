a8525fb4528e954a738f312af6b2a4f1669e9152
chich2 <bamtorrr@gmail.com>
2021-01-12 09:50:54 +0900

    210112

---
+++ baekjoon/OX_quiz.py
@@ -0,0 +1,22 @@
+# 8958
+# str 을 list 처럼 다루면 자동으로 list로 사용된다.
+
+# 입력
+num_test = int(input())
+
+scr = 0
+cnt_o = 1
+
+for tc in range(num_test):
+    str = input()
+    for char in str:
+        if(char == 'O'):
+            scr += cnt_o
+            cnt_o += 1
+        else:
+            cnt_o = 1
+    print(scr)
+    scr = 0
+    cnt_o = 1
+
+
