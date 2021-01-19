5c22f4dcdb8bae65771299e9575cdf80f62bbb6f
chich2 <bamtorrr@gmail.com>
2021-01-19 18:53:47 +0900

    재원이꺼참고, sum이 왜 적용 안되는지 파악 요망

--- baekjoon/self_num.py
+++ baekjoon/self_num.py
@@ -2,15 +2,19 @@
 
 def make_not_self_num(num):
     pre_num = num
-    list_num = list(map(int, str(num).split()))
-    sum_num = int(pre_num) + sum(list_num)
-    return sum_num
+    # list_num = list(map(int, str(num).split()))
+    # sum_num = int(pre_num) + sum(list_num)
+
+    for i in list(str(num)):
+        pre_num += int(i)
+    return pre_num
 
 
 not_self_nums = []
 for cnt in range(1, 10001):
-    not_self_nums.append(make_not_self_num(cnt))
+    not_self_num = make_not_self_num(cnt)
+    not_self_nums.append(not_self_num)
 
 for num in range(1, 10001):
     if num not in not_self_nums:
-        print(num)
+        print("{0}".format(num))
