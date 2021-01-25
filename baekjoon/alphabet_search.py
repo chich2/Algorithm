7f5fe35ed7c51ecfcb79614af6b97e6e666d1437
chich2 <bamtorrr@gmail.com>
2021-01-25 23:57:45 +0900

    에러해결, print부분만보완요망

---
+++ baekjoon/alphabet_search.py
@@ -0,0 +1,15 @@
+# 10809
+
+# -1로된 리스트 생성
+list_alpha = []
+for i in range(26):
+    list_alpha.append(-1)
+
+
+# 단어 받아서 변환시키기
+word = list(input())
+for idx_word in range(0, len(word)):
+    idx_alpha = ord(word[idx_word]) - 97
+    list_alpha[idx_alpha] = idx_word
+
+# print
